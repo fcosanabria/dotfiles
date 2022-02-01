7b72515a2a05d47fbb16b2e2cf74bcfd3db3dcf2
Francisco Sanabria <piktonus97x@hotmail.com>
2022-01-31 20:48:19 -0600

    Quick Update, to refer to the Vim configuration file

---
+++ popos-inventory.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Esto son los programas que he estado instalando en mi PopOS de uso diario
+
+# zsh, ya que me en macos la uso, se me hace mas familiar, ya que paso mas tiempo en zsh, ademas de que es mas amigable.
+sudo apt install zsh
+
+chsh -s $(which zsh)
+
+# Solamente si es Ubuntu based disto. No recuerdo si PopOs lo tiene instalado por defecto, pero por si las dudas. 
+sudo apt install git 
+
+# nvim install and configuration 
+sudo apt install nvim
+# nvim configuration 
+Copiar de la Mac el configuration file.
+
+
