656a364e3619a199814c4d52d544eaba82903bc6
Francisco Sanabria <piktonus97x@hotmail.com>
2023-01-23 12:46:38 -0600

    adding some changes

---
+++ nvim/lua/piktonus/plugins/nvim-tree.lua
@@ -0,0 +1,19 @@
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
+-- recommended settings fro nvim-tree documentation
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+nvimtree.setup({
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+})
---
+++ nvim/lua/piktonus/plugins/treesitter.lua
@@ -0,0 +1,42 @@
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+-- configure treesitter
+treesitter.setup({
+	-- enable syntax highlighting
+	highlight = {
+		enable = true,
+	},
+	-- enable indentation
+	indent = { enable = true },
+	-- enable autotagging (w/ nvim-ts-autotag plugin)
+	autotag = { enable = true },
+	ensure_installed = {
+		"json",
+		"javascript",
+		"typescript",
+		"tsx",
+		"yaml",
+		"html",
+		"css",
+		"markdown",
+		"svelte",
+		"graphql",
+		"bash",
+		"lua",
+		"vim",
+		"dockerfile",
+		"gitignore",
+		-- mine
+		"cpp",
+		"go",
+		"hcl",
+		"java",
+		"python",
+	},
+	-- auto install above language parsers
+	auto_install = true,
+})
