8453acb7c855e0f595291be3e9574d5505d289d3
Jose Francisco Sanabria Almendarez <jsanabri@cr.ibm.com>
2023-01-25 12:02:58 -0600

    adding copilot to nvim plugins using packer

--- config/nvim/lua/piktonus/plugins-setup.lua
+++ config/nvim/lua/piktonus/plugins-setup.lua
@@ -96,6 +96,9 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+	-- GitHub Copilot
+	use("github/copilot.vim")
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
