38a1d070da4a6e2c25f11b6cebd8f3e9a25c7c4c
Francisco Sanabria <francisco.sanabria@datasite.com>
2024-04-30 14:08:21 -0600

    Modified some apps that I don't use anymore.

--- inventory/macos/initial-configuration.sh
+++ inventory/macos/initial-configuration.sh
@@ -5,25 +5,9 @@
 defaults write -g ApplePressAndHoldEnabled -bool false 
 
 # ZSH Configuration
-ln -s $HOME/git/dotfiles/zsh-config/M1/zshrc $HOME/.zshrc
+ln -s $HOME/git/dotfiles/zsh/zshrc $HOME/.zshrc
 
 mkdir -p $HOME/.config/nvim/
 
 # Installing Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# npm pre-confiration for Lunavim
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-
-if [ -f ~/.profile ]; then
-  # If the file exists, append the export line to the end of the file
-  cat << EOF >> ~/.profile
-export PATH=~/.npm-global/bin:$PATH
-EOF
-else
-  # If the file doesn't exist, create it and write the export line
-  echo "export PATH=~/.npm-global/bin:$PATH" > ~/.profile
-fi
-
-source ~/.profile
