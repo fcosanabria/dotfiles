a416f88b0846cb8c0b8c9ea4ff13f9b60bec2d88
Digital Death Disrupt <37163970+digitaldeathdisrupt@users.noreply.github.com>
2024-10-15 22:03:09 -0600

    Adding more changes for the incomming new version

---
+++ inventory/macos/installing-apps.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Homebrew
+brew install fzf
+brew install bat
+brew install lsd
+brew install ripgrep
+brew install htop
+brew install lazygit
+brew install tldr
+brew install tree
+brew install wget
+brew install neovim
+brew install azure-cli
+brew install starship
+brew install docker
+brew install mas # Mac App Store CLI
+
+# Casks
+brew install --cask monarch
+brew install --cask devonthink
+brew install --cask appcleaner
+brew install --cask affinity-photo@1
+brew install --cask affinity-designer@1
+brew install --cask bruno
+brew install --cask audio-hijack
+brew install --cask loopback
+brew install --cask betterdisplay
+brew install --cask bettertouchtool
+brew install --cask bitwarden
+brew install --cask bootstrap-studio
+brew install --cask cleanshot
+brew install --cask cold-turkey-blocker
+brew install --cask dangerzone
+brew install --cask jordanbaird-ice
+brew install --cask ticktick
+brew install --cask discord
+brew install --cask duplicacy-web-edition
+brew install --cask ogdesign-eagle
+brew install --cask handbrake
+brew install --cask iina
+brew install --cask iterm2
+brew install --cask karabiner-elements
+brew install --cask keyboardcleantool
+brew install --cask keycastr
+brew install --cask obs
+brew install --cask obsidian
+brew install --cask qmk-toolbox
+brew install --cask rectangle-pro
+brew install --cask spotify
+brew install --cask stremio
+brew install --cask uhk-agent
+brew install --cask visual-studio-code
+brew install --cask brave-browser
+brew install --cask orion
+
+# App Store
+mas install 1092899208 # color ui
+mas install 1515453657 # Asustor Control Center
+mas install 1024640650 # CotEditor
+mas install 6476452351 # DevHub
+mas install 470158793 # Keka
+mas install 1538878817 # UTM
+mas install 1614730313 # MusicBox
+mas install 1298486723 # FileZilla Pro
+mas install 715464874 # Disk Map
+mas install 955848755 # Theine
+mas install 1289583905 # Pixelmator Pro
+mas install 1507246666 # Presentify
+mas install 1477089520 # Backtrack
+mas install 568827824 # Frax
