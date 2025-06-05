89b0844b43bf73338433a295e60449b2af5127a7
Francisco Sanabria <fcosanabria@pm.me>
2025-06-05 17:42:47 -0600

    feat: despues de la tragedia, voy a empezar a documentar los cambios que estoy haciendo en mis sistema con respecto a paquetes

---
+++ arch-install-packages.sh
@@ -0,0 +1,13 @@
+docker
+docker-engine
+git-delta
+lazygit
+bat
+eza
+zoxide
+kubectl
+k9s
+paru -S oxker
+# DevPod
+curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
+wget https://gitlab.com/kyb/fish_ssh_agent/raw/master/functions/fish_ssh_agent.fish -P ~/.config/fish/functions/
