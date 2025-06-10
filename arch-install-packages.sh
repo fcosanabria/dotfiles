920def82ca52d18c50d8abb8f0973f4faadfeac9
Francisco Sanabria <fcosanabria@pm.me>
2025-06-10 12:57:38 -0600

    feat: adding more packages

--- arch-install-packages.sh
+++ arch-install-packages.sh
@@ -15,3 +15,6 @@ paru -S oxker
 curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
 fisher install thernstig/fish-ssh-agent
 npm
+atac
+kanata
+netscanner
