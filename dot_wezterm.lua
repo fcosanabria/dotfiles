c6d95c9da7f4b0cc93391edc8f3dc1fe5c6944dc
Francisco Sanabria <francisco.sanabria@datasite.com>
2025-10-29 12:09:08 -0600

    Adding wezterm configuration from my macos machine @work

---
+++ dot_wezterm.lua
@@ -0,0 +1,58 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- general
+
+config.font_size = 18
+config.font = wezterm.font("Berkeley Mono")
+config.force_reverse_video_cursor = true
+config.colors = {
+	foreground = "#c5c9c5",
+	background = "#181616",
+
+	cursor_bg = "#C8C093",
+	cursor_fg = "#C8C093",
+	cursor_border = "#C8C093",
+
+	selection_fg = "#C8C093",
+	selection_bg = "#2D4F67",
+
+	scrollbar_thumb = "#16161D",
+	split = "#16161D",
+
+	ansi = {
+		"#0D0C0C",
+		"#C4746E",
+		"#8A9A7B",
+		"#C4B28A",
+		"#8BA4B0",
+		"#A292A3",
+		"#8EA4A2",
+		"#C8C093",
+	},
+	brights = {
+		"#A6A69C",
+		"#E46876",
+		"#87A987",
+		"#E6C384",
+		"#7FB4CA",
+		"#938AA9",
+		"#7AA89F",
+		"#C5C9C5",
+	},
+}
+
+-- window
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+
+-- Key binding
+config.keys = {
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+}
+
+return config
